73e0406d37188a3cf93d0f3f06f73b6ccc49238c
amagana10 <32153064+amagana10@users.noreply.github.com>
2020-10-04 11:56:10 -0500

    Update AccountHolder.java

--- src/main/java/com/meritamerica/assignment1/AccountHolder.java
+++ src/main/java/com/meritamerica/assignment1/AccountHolder.java
@@ -85,10 +85,10 @@ public class AccountHolder {
 	public String toString() {
 		return "Name: " + this.firstName + this.middleName + this.lastName +"\n"
 				+ "SSN: " + this.ssn +"\n"
-				+ "Checking Account Balance: " + this.cAccount.getBalance() +"\n"
+				+ "Checking Account Balance: $" + this.cAccount.getBalance() +"\n"
 				+ "Checking Account Interest Rate: " + this.cAccount.getInterestRate() +"\n"
 				+ "Checking Account Balance in " + YEARS + " years: $" + this.cAccount.futureValue(YEARS) +"\n"
-				+ "Savings Account Balance: "+ this.sAccount.getBalance() +"\n"
+				+ "Savings Account Balance: $"+ this.sAccount.getBalance() +"\n"
 				+ "Savings Account Interest Rate: "+ this.sAccount.getInterestRate() +"\n"
 				+ "Savings Account Balance in " + YEARS + " years: $"+ this.sAccount.futureValue(YEARS) +"\n";
 	}
